31c5506b18d40c90b09e2bd0664d97bb44880069
rubygeraldyne22 <rubygeraldyne22@gmail.com>
2022-04-16 20:58:34 +0700

    exercise 6

---
+++ Exercise-05/# data mahasiswa.py
@@ -0,0 +1,13 @@
+# data mahasiswa
+print ("123345678901234567890")
+print ("==========================")
+print ("Tabel konversi fiska 1 TI UNS")
+print (" Kilogra,        Pounds")
+print ("==========================")
+for kg in range (0, 20) :
+    kg += 1 
+    lb =2.2
+    lb=kg* lb
+    print(format(kg,"5"), end="")
+    print(format(lb,"14.2f"))
+print ("==========================")
