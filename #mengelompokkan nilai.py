a5760659b16be693a0dbdcddb2682b47230e3598
rubygeraldyne22 <rubygeraldyne22@gmail.com>
2022-05-24 20:37:52 +0700

    abc

--- #mengelompokkan nilai.py
+++
@@ -1,9 +0,0 @@
-#mengelompokkan nilai
-print("==================")
-nilai=int(print("Masukkan nilai mata kuliah anda= "))
-if nilai => 80:
-    print("grade A")
-elif nilai => 40:
-    print("grade B")
-else:
-    print("tidak lulus")
